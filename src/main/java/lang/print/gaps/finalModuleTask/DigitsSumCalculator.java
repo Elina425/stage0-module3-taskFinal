496b6cda31db2350f0a38e830095e9b72e05f685
Elina <elina.melkonyan.y@tumo.org>
2022-08-02 01:30:31 +0400

    45

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int thousand = number/1000;//4
+        int thousands = number %1000;//225
+        int hundred = thousands /100;//2
+        int hundreds = thousands %100;//25
+        int ten = hundreds /10;//2
+        int tenth = hundreds %10;//5
+        int sum = thousand + hundred + ten + tenth;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int original = number/10;//10
+        int oremainder = number %10;//24
+        int tenth = oremainder/10;//2
+        int remainder = oremainder % 10;//4
+         int reverse = remainder * 100 + tenth *10 + original;
+        System.out.println(reverse);
     }
 }
